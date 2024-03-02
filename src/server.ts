a7a85c5fce7ca1d60fdd02329b318483aeebe7c8
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2024-03-02 19:55:21 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -7,21 +7,25 @@ import transactionRoutes from "./routes/transactionRoutes";
 require('dotenv').config()
 
 //app setup
+
 const app = express();
 const port = 8001;
 const connection_url = process.env.MONGODB_URL;
 
 // middleware 
+
 app.use(express.json());
 app.use(Cors());
 
 // DB Config
+
 mongoose.connect(connection_url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 } as any);
 
 // API Endpoints
+
 app.use('/api/account', accountRoutes);
 app.use('/api/txn', transactionRoutes);
 
